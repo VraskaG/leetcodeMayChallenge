4e81d4b9e01d44590d8a76d862cb2b6c2890b261
Shantanu Malik <32797297+VraskaG@users.noreply.github.com>
2020-05-05 17:44:43 +0530

    Create NumberComplement.cpp

---
+++ NumberComplement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   int findComplement(int num) {
+	int binaryNum[32], i;
+	for(i = 0; num > 0; i++)
+	{
+		binaryNum[i] = num % 2;
+		num = num / 2;
+	}
+	reverse(binaryNum, binaryNum + i);
+	for (int j = 0; j < i; j++)
+	{
+		if (binaryNum[j] == 1)
+			binaryNum[j] = 0;
+		else
+			binaryNum[j] = 1;
+	}
+	int x = 0, N = 0;
+	while (i--)
+	{
+		x += binaryNum[i] * pow(2, N);
+		N++;
+	}
+	return x;
+}
+};
