60773001cefdc70a139f8760c5bf02f39e28df62
Shantanu Malik <32797297+VraskaG@users.noreply.github.com>
2020-05-06 17:25:30 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<int, int> Occurence;
         for(int i = 0; i < nums.size(); i++)
         {
