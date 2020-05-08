dd16da412deafd7e5393389fc3d8d9b7e8da3f04
Shantanu Malik <32797297+VraskaG@users.noreply.github.com>
2020-05-08 19:58:54 +0530

    Create Day8.cpp

---
+++ Day8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+   int gcd(int a, int b){
+      return !b?a:gcd(b,a%b);
+   }
+   bool checkStraightLine(vector<vector<int>>& coordinates) {
+        if(coordinates.size() <= 2)
+             return 1;
+        int xn = coordinates[0][0] - coordinates[1][0];
+        int yn = coordinates[0][1] - coordinates[1][1];
+        for(int i=2; i < coordinates.size(); i++){
+		    int x = coordinates[i][0];
+            int y = coordinates[i][1];   
+		    if( xn*(y - coordinates[0][1]) != yn*(x - coordinates[0][0]) )
+			        return false;
+	    }
+	    return true;
+   }
+};
