4b563d371c751b85e1f8db51e71b1b43211d4183
Shantanu Malik <32797297+VraskaG@users.noreply.github.com>
2020-05-10 22:35:35 +0530

    Update Day10.cpp

--- Day10.cpp
+++ Day10.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findJudge(int N, vector<vector<int>>& trust) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int counter[1001]={0};
         for ( auto x : trust){
             counter[x[0]]--;
