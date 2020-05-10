ad50ce170e679d3562dbd82af4e43a1daf1b49f8
Shantanu Malik <32797297+VraskaG@users.noreply.github.com>
2020-05-10 11:09:30 +0530

    Update Day9.cpp

--- Day9.cpp
+++ Day9.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         for (long long int i = 1; i * i <= num; i++) { 
             if ((num % i == 0) && (num / i == i)) { 
                 return true; 
